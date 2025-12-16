968e20896d35e8f81f355e34bbb56c23ccfd1dc6
DavidB <david.bernard@fms-ea.com>
2025-12-16 17:41:11 +0100

    first commit thread exercice

---
+++ src/thread/Thread.java
@@ -0,0 +1,13 @@
+package thread;
+
+public class Thread {
+    private String name;
+
+    public Thread(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
